afa45b10c28efb339059b552da6ddced5d42a224
Ritersport <leomkrita2001@gmail.com>
2026-07-06 21:20:17 +0700

    +

--- features/TransactionList/build.gradle.kts
+++ features/TransactionList/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.android.built.in1.kotlin)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.android.hilt)
 }
 
 android {
@@ -30,11 +32,13 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.foundation)
     implementation(libs.material)
     implementation(libs.ui.tooling.preview)
     implementation(libs.dagger.hilt)
     implementation(libs.androidx.hilt.navigation.compose)
     ksp(libs.hilt.android.compiler)
 
-    api(":items:TransactionCard")
+    implementation(project(":core:DataBase"))
+    implementation(project(":items:TransactionCard"))
 }
