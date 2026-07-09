f840d38253eb1e267f045ac132094d95b5f8c5eb
Ritersport <leomkrita2001@gmail.com>
2026-07-09 21:26:10 +0700

    TransactionList

--- features/TransactionList/src/main/java/com/ritesrport/transactionlist/TransactionList.kt
+++ features/TransactionList/src/main/java/com/ritesrport/transactionlist/TransactionList.kt
@@ -1,9 +1,64 @@
 package com.ritesrport.transactionlist
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.ritesrport.items.transactioncard.TransactionCard
+import com.ritesrport.items.transactioncard.TransactionInterface
+import com.ritesrport.items.transactioncard.TransactionModel
+import com.ritesrport.items.transactioncard.transactionInterfacePreview
+import com.ritesrport.items.transactioncard.transactionModelPreview
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 @Composable
-fun TransactionList(modifier: Modifier = Modifier) {
+fun TransactionListScreen(
+    viewModel: TransactionListViewModel = hiltViewModel(),
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+    when (state) {
+        is TransactionListUiState.Error -> TODO()
+        TransactionListUiState.Loading -> Text("Loading...")
+        is TransactionListUiState.Success -> TransactionListLayout(
+            (state as TransactionListUiState.Success).items,
+            viewModel
+        )
+    }
+}
 
+@Composable
+fun TransactionListLayout(
+    transactionItems: List<TransactionModel>,
+    transactionCardInterface: TransactionInterface,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn() {
+        itemsIndexed(transactionItems) { index, item ->
+            TransactionCard(
+                item,
+                transactionCardInterface,
+            )
+            if (transactionItems.lastIndex != index) {
+                HorizontalDivider()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TransactionListLayoutPreview(modifier: Modifier = Modifier) {
+    TransactionListLayout(
+        listOf(
+            transactionModelPreview,
+            transactionModelPreview,
+            transactionModelPreview
+        ),
+        transactionInterfacePreview
+    )
 }
---
+++ features/TransactionList/src/main/java/com/ritesrport/transactionlist/TransactionListViewModel.kt
@@ -0,0 +1,42 @@
+package com.ritesrport.transactionlist
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ritesrport.items.transactioncard.TransactionInterface
+import com.ritesrport.items.transactioncard.TransactionModel
+import com.ritesrport.items.transactioncard.transactionModelPreview
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class TransactionListViewModel @Inject constructor(
+   // private val transactionsRepository: TransactionsRepository
+) : ViewModel(), TransactionInterface {
+
+    val state: StateFlow<TransactionListUiState>
+        get() = _state
+
+    private val _state: MutableStateFlow<TransactionListUiState> =
+        MutableStateFlow(TransactionListUiState.Loading)
+
+    init {
+        viewModelScope.launch {
+            delay(3000)
+            _state.value = TransactionListUiState.Success(listOf(transactionModelPreview))
+        }
+    }
+
+    override fun onTransactionCardClick(transactionId: Long) {
+
+    }
+}
+
+sealed interface TransactionListUiState {
+    object Loading : TransactionListUiState
+    data class Error(val throwable: Throwable) : TransactionListUiState
+    data class Success(val items: List<TransactionModel>) : TransactionListUiState
+}
